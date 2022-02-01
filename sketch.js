62429878ecc59eee277f6f8be174ce57f07ce4c0
N00190737 <91547890+N00190737@users.noreply.github.com>
2022-02-01 15:28:17 +0000

    New Console Message

--- sketch.js
+++ sketch.js
@@ -5,6 +5,6 @@ clap(3,8);
 function clap(startnum, endnum){
     let loopCount = endnum - startnum;
     for(let i=0; i<=loopCount; i++){
-    console.log("Wagwan G " + ( startnum+i))
+    console.log("Whats the craic " + ( startnum+i))
     }
 }
